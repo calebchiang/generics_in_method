d85608ff0f3216f97a21c454a8fa94b19086a4eb
Caleb Chiang <calebchiang1002@gmail.com>
2024-06-06 13:40:01 -0700

    Added function for instances of Point that are type f32.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ impl<T> Point<T> {
     }
 }
 
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 { // Method is only available for instances of Point that are type f32
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
 fn main() {
     let point = Point {
         x: 3.0,
